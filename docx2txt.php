67c70a648bb72589a6c16bd4d159b7723959098c
ismael-miguel <espertalhao04@hotmail.com>
2015-06-17 20:38:48 +0100

    Create docx2txt.php

---
+++ docx2txt.php
@@ -0,0 +1,65 @@
+<?php
+	
+	if( $args[1] )
+	{
+		$file = $args[1];
+	}
+	else if( ( $stdin = fopen( 'php://stdin','rb' ) ) && stream_set_blocking( $stdin, 0 ) && fread( $stdin, 1) )
+	{
+		//we read one byte, we must read that byte again
+		fseek( $stdin, 0 );
+		
+		if( strtoupper( substr( PHP_OS, 0, 3 ) ) === 'WIN' )
+		{
+			$file = tempnam( '%temp%', 'x2T' );
+		}
+		else
+		{
+			$file = tempnam( '/tmp', 'x2T' );
+		}
+		
+		//keeping compatibility with older versions
+		register_shutdown_function( create_function( '$file', 'unlink( $file );' ), $file );
+		
+		//without b, windows will interpret it as text
+		$tmp_file = fopen( $file, 'w+b' );
+		
+		fwrite( $tmp_file, fread( $stdin ) );
+		
+		fclose( $stdin );
+		fclose( $tmp_file );
+		unlink( $tmp_file );
+	}
+	else
+	{
+		trigger_error( 'Error: a file on STDIN or a filename as the first argument', E_USER_ERROR );
+	}
+	
+	//docx files are just a zipped file
+	$zip = new ZipArchive();
+	
+	if ( $zip->open( $file ) === true )
+	{
+		//this is where all the content is present
+		if( $xml = $zip->getFromName( 'word/document.xml' ) )
+		{
+			$dom = new DOMDocument( '1.0', 'utf-8' );
+			$dom->loadXML( $xml );
+
+			//the namespace is actually w, the tag is t. text is in <w:t> elements
+			for($i = 0, $list = $dom->getElementsByTagNameNS( '*', 't' ); $i < $list->length; $i++ )
+			{
+				echo $list[$i]->nodeValue, PHP_EOL;
+			}
+			
+			$zip->close();
+		}
+		else
+		{
+			trigger_error( 'Invalid structure for the file "' . $args[1] . '"' );
+		}
+	}
+	else
+	{
+		trigger_error( 'Failed to open the file "' . $args[1] . '"', E_USER_ERROR );
+	}
